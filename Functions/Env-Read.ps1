f141eece110a5c08d01155c97eda53ba17fd2af4
shogun <iivchenko@live.com>
2016-07-29 23:01:53 +0100

    Implement Env Functions
    Env-Read
    Env-Write
    
    Issue #2

--- Functions/Env-Read.ps1
+++
@@ -1,6 +0,0 @@
-# TODO
-# Issue: https://github.com/iivchenko/PowerEnv/issues/2
-
-# Env-Read -Property <Name>
-# Reads specified Property from the PowerEnv.config file
-# If file doesn't exit - return null or empty
---
+++ Functions/Env.ps1
@@ -0,0 +1,93 @@
+﻿function ConfigPath
+{
+    return (Get-Item $PSScriptRoot).Parent.FullName + "\PowerEnv.config"
+}
+
+<#
+.SYNOPSIS    
+    Writes property to the PowerEnv.config file.
+
+.DESCRIPTION
+    If property exists it will be reweriten. If file doesn't exist it will be created.
+
+.PARAMETER Property
+    Name of the property to save.
+
+.PARAMETER Value
+     Value for the property to save.
+
+.EXAMPLE
+    Env-Write -Propery age -Value 55
+
+    Writes property with the name 'age' and value '55'
+#>
+function Env-Write 
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+	    [string]$Property,
+        
+        [Parameter(Mandatory=$true)]
+        [string]$Value
+    )
+    
+    $configPath = ConfigPath
+    
+    if (Test-Path $configPath)
+    {
+        $settigns = Import-Clixml -Path $configPath        
+    }
+    else
+    {
+        $settigns = @{}
+    }
+
+    if($settigns.Contains($Property))
+    {
+        $settigns[$Property] = $Value
+    }
+    else
+    {
+        $settigns.Add($Property, $Value)
+    }
+
+    $settigns | Export-Clixml -Path $configPath    
+}
+
+<#
+.SYNOPSIS    
+    Reads property from the PowerEnv.config file.
+
+.DESCRIPTION
+    If property exists it will return it's value if not null will be returned. If file doesn't exist the null will be returned.
+
+.PARAMETER Property
+    Name of the property read.
+
+.EXAMPLE
+    Env-Read -Propery age
+
+    Reads value of the property 'age'
+#>
+function Env-Read 
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+	    [string]$Property
+    )
+    
+    $configPath = ConfigPath
+    
+    if (Test-Path $configPath)
+    {
+        $settigns = Import-Clixml -Path $configPath
+
+        return $settigns[$Property]
+    }
+    else
+    {
+        return
+    }
+}
