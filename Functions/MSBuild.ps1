ff801670a9220b27cde465d3b150955d381f410a
shogun <iivchenko@live.com>
2016-07-29 23:23:19 +0100

    Implement MSBuild
    Issue #3

---
+++ Functions/MSBuild.ps1
@@ -0,0 +1,15 @@
+﻿function MSBuild-GetPath
+{
+    return Env-Read -Property MSBuildExePath
+}
+
+function MSBuild-SetPath
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+	    [string]$Path        
+    )
+    
+    Env-Write -Property MSBuildExePath -Value $Path
+}
