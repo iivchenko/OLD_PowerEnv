4f4dbec35bb694e709fd3f12b80db11d69754645
shogun <iivchenko@live.com>
2016-07-29 20:30:47 +0100

    Add TODOs
    Issue #1

---
+++ Functions/Git-GetPath.ps1
@@ -0,0 +1,4 @@
+# TODO
+# Issue: https://github.com/iivchenko/PowerEnv/issues/5
+
+# Use Env-Read.ps1
---
+++ Functions/MSBuild-SetPath.ps1
@@ -0,0 +1,4 @@
+# TODO
+# Issue: https://github.com/iivchenko/PowerEnv/issues/3
+
+# Use Env-Write.ps1
