5cbba69ae88f21d6e4a67c8e3a391984e7107848
shogun <iivchenko@live.com>
2016-07-31 18:58:55 +0100

    Implement Git
    Issue #5

--- Functions/Git-DoAll.ps1
+++
@@ -1,8 +0,0 @@
-# TODO
-# Issue: https://github.com/iivchenko/PowerEnv/issues/5
-
-# Use old all.ps1 script
-# Git-DoAll -Command <command> -RepoPaths <path1>;<path2>
-# Git-DoAll -Command <command> -MaxDepth 1
-# Git-DoAll -Command <command> 
-# If -RepoPaths doesn't specified use current dir and search all repos 
