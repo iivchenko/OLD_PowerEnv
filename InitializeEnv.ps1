1680be9cd29fc0148804aa9bf795eb7be1b22097
shogun <iivchenko@live.com>
2016-07-31 21:12:31 +0100

    Improve InitializeEnv.ps1

--- InitializeEnv.ps1
+++ InitializeEnv.ps1
@@ -8,9 +8,6 @@ Clear-Host
 
 Write-Host "`nCopyright (c) 2016 by Shogun, All Right Reserved`nDon't be shy just enjoy with PowerEnv =)`n" -ForegroundColor Green
 
-# Set working directory.
-# Env-Read -Property WorkingDir | Set-Location
-
 # Provide direct access to scripts.
 $scriptsPath = (Get-Item "$PSScriptRoot\Scripts").FullName
 $env:Path = "$env:Path;$scriptsPath"
@@ -20,5 +17,14 @@ $env:Path = "$env:Path;$scriptsPath"
 Get-ChildItem "$PSScriptRoot\Functions" | % { & $_.FullName }
 
 # Set useful aliaces.
+# But first set notepad++ binaries path manualy. Example: Env-Write -Property NotepadPlusPath -Value "C:\Program Files (x86)\Notepad++\notepad++.exe"
 # Set-Alias -Name edit -Scope Global -Value (Env-Read NotepadPlusPath)
+
+# Set Executable paths manuly
+# Git: Env-Write -Property GitExePath -Value "C:\Program Files\Git\bin\git.exe"
+# MSBuild: Env-Write -Property MSBuildExePath -Value "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
+
+# Set working directory.
+# But first set path manualy. Example: Env-Write -Property WorkingDir -Value "D:\Sources\"
+# Env-Read -Property WorkingDir | Set-Location
 
