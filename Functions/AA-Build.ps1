e272f687e3581d021508bebe2e0d78e9a1f8ffc5
shogun <iivchenko@live.com>
2016-07-31 17:50:42 +0100

    Implement AA
    Issue #4

--- Functions/AA-Build.ps1
+++
@@ -1,7 +0,0 @@
-# TODO
-# Issue: https://github.com/iivchenko/PowerEnv/issues/4
-
-# Use old build.ps1 script
-# Add -Path parameter. If Path is not set use current execution path
-# Read MSBuild path from PowerEnv.config file
-
---
+++ Scripts/AABuildReleaseX64.ps1
@@ -0,0 +1,3 @@
+Param([string] $Path)
+
+AABuild.ps1 -Configurations Release -Platforms x64,"any cpu" -Path $Path
